3c7c69b804519bb10d405c60ac5ba0dcac28487d
Hugi <hugiasgeirsson@gmail.com>
2017-01-18 15:36:22 +0000

    Added event ID to camps, added a Application config variable for current event id and added filtering to Dream index to only show Dreams with current event ID

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170114112133) do
+ActiveRecord::Schema.define(version: 20170118145316) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     :index=>{:name=>"index_active_admin_comments_on_namespace"}
@@ -147,6 +147,7 @@ ActiveRecord::Schema.define(version: 20170114112133) do
     t.boolean  "projectmgmt_is_theme_camp_dream",                          :default=>false, :null=>false
     t.boolean  "projectmgmt_is_dream_near_theme_camp",                     :default=>false, :null=>false
     t.string   "projectmgmt_dream_pre_construction_site",                  :limit=>4096
+    t.string   "event_id",                                                 :limit=>128, :default=>"borderland2017"
   end
 
   create_table "grants", force: :cascade do |t|

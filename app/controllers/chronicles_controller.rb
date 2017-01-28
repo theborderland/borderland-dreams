1e4b333107e2b4bd9fda6fb734c53a22ece06ad5
Hugi <hugiasgeirsson@gmail.com>
2017-01-28 23:05:59 +0100

    Merged new Midburn features

--- app/controllers/chronicles_controller.rb
+++ app/controllers/chronicles_controller.rb
@@ -1,10 +1,9 @@
-class HowcanihelpController < ApplicationController
+class ChroniclesController < ApplicationController
   def index
     filter = params[:filterrific] || { sorted_by: 'updated_at_desc' }
     filter[:active] = true
     filter[:not_hidden] = true
-    filter[:is_cocreation] = true
-    filter[:is_current_event] = true
+    filter[:is_current_event] = false
     if (!current_user.nil? && (current_user.admin? || current_user.guide?))
       filter[:hidden] = true
       filter[:not_hidden] = false

e7a11ff7ed2e7a11e7ab5fd837074a7464a0ef96
Hugi <hugiasgeirsson@gmail.com>
2017-01-29 03:49:54 +0100

    Revert "Merge remote-tracking branch 'Midburn/master' into sync-with-midburn-28012016"
    This reverts commit 31d326f40ead31fcd513da3cd048efe23bbb1b96, reversing
    changes made to fff275e8c50ce91e5ea06379009c9c5d3718ddf7.

--- db/migrate/20170122213524_add_has_ticket_and_early_arrival_to_person.rb
+++
@@ -1,6 +0,0 @@
-class AddHasTicketAndEarlyArrivalToPerson < ActiveRecord::Migration
-  def change
-    add_column :people, :has_ticket, :boolean
-    add_column :people, :needs_early_arrival, :boolean
-  end
-end

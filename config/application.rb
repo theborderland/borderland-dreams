e3634edd20aba260b8c5216f5b4ea3040d99be66
Hugi Ásgeirsson <hugiasgeirsson@gmail.com>
2017-05-02 22:46:08 +0200

    Added max budget for grants

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Firestarter
     config.active_record.raise_in_transactional_callbacks = true
     Rails.application.config.x.firestarter_settings = config_for(:firestarter_settings)
     Rails.application.config.coin_rate = 50 #Coin conversion from DKK
-    Rails.application.config.maxbudget = ENV['MAXTOTALGRANTS'] or false
+    Rails.application.config.maxbudget = 10000 # Max art grant budget
     Rails.application.config.default_event = "borderland2017" #Coin conversion from DKK    
 
     config.autoload_paths += Dir["#{config.root}/lib/googleAppsScript/**/"]

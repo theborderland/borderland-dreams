b557e72bab963dadf8083aab3dd7956c1fce825e
Christophe Bram <cbramdit@gmail.com>
2018-02-20 00:49:34 +0100

    Users can grant N hearts max to any given dream

--- app/models/camp.rb
+++ app/models/camp.rb
@@ -160,7 +160,7 @@ class Camp < ActiveRecord::Base
 
   def grants_received
     return self.grants.sum(:amount)
-  end 
+  end
 
   # Translating the real currency to budget
   # This called on create and on update
@@ -169,7 +169,7 @@ class Camp < ActiveRecord::Base
     if self.minbudget_realcurrency.nil?
       self.minbudget = nil
     elsif self.minbudget_realcurrency > 0
-      self.minbudget = (self.minbudget_realcurrency / Rails.application.config.coin_rate).ceil
+      self.minbudget = (self.minbudget_realcurrency / Rails.configuration.x.firestarter_settings["coin_rate"]).ceil
     else
       self.minbudget = 0
     end
@@ -177,7 +177,7 @@ class Camp < ActiveRecord::Base
     if self.maxbudget_realcurrency.nil?
       self.maxbudget = nil
     elsif self.maxbudget_realcurrency > 0
-      self.maxbudget = (self.maxbudget_realcurrency / Rails.application.config.coin_rate).ceil
+      self.maxbudget = (self.maxbudget_realcurrency / Rails.configuration.x.firestarter_settings["coin_rate"]).ceil
     else
       self.maxbudget = 0
     end

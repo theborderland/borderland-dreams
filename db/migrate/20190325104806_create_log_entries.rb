9a2d27065afb6589ae70eda3e7afc0e1a5f294fe
Kristoffer Ravneberg <kristoffer@microdisko.no>
2019-03-25 12:39:34 +0100

    wip

---
+++ db/migrate/20190325104806_create_log_entries.rb
@@ -0,0 +1,14 @@
+class CreateLogEntries < ActiveRecord::Migration[5.0]
+  def change
+    create_table :log_entries do |t|
+      t.timestamps
+      t.string :topic
+      t.string :type
+      t.integer :user_id
+      t.string :user_name
+      t.integer :object_id
+      t.string :object_name
+      t.string :description
+    end
+  end
+end

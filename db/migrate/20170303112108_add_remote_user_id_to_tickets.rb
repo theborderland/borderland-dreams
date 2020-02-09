2722deaba02f8100898a543d110944d75997a765
Max Gnipping <max@farx.io>
2020-02-09 11:49:32 +0100

    More merge issues fixed

--- db/migrate/20170303112108_add_remote_user_id_to_tickets.rb
+++
@@ -1,5 +0,0 @@
-class AddRemoteUserIdToTickets < ActiveRecord::Migration
-  def change
-    add_column :tickets, :remote_user_id, :integer
-  end
-end
--- db/migrate/20170502215129_add_default_grants_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddDefaultGrantsToUser < ActiveRecord::Migration
-  def change
-    change_column :users, :grants, :integer, { :default => 10 }
-  end
-end
